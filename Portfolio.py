5129b6c09364691b6c8119f3e2809669728ce19e
Vandit <52314194+vanditkhurana@users.noreply.github.com>
2019-10-30 14:21:08 +0530

    Portfolio.py

---
+++ Portfolio.py
@@ -0,0 +1,13 @@
+name="Vandit"
+print("Name : ",name)
+branch="CSE"
+print("Branch : ",branch)
+Skills=['Python', 'C Programming', 'HTML']
+print("Skills : ")
+for skill in Skills:
+  print("   ",skill)
+Hobbies=['Watching American TV Shows', 'Playing Guitar', 'Singing']
+print("Hobbies : ")
+for hobby in Hobbies:
+  print("   ",hobby)
+print("-------Thank You-------")
