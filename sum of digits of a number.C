2abda190370dc1f2b692c162671de39db75a1e3b
anmolsingla <57210856+anmolsingla@users.noreply.github.com>
2019-10-31 12:53:50 +0530

    updates in sum of digits
    errors in syntax and logic

--- sum of digits of a number.C
+++ sum of digits of a number.C
@@ -1,16 +1,14 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
-  int a,b,
-  printf("Enter a no:")
-  scanf("%d", a);
+  int a,b;
+  printf("Enter a no:");
+  scanf("%d", &a);
   for(a!=0)
-  {
+   {
    b=a%10;
    sum=sum+b;
-   a=a/10
+   a=a/10;
    }
-   print("Sum of digits is %d",sum);
-   getch();
-   }
+   printf("Sum of digits is %d",sum);
+}
