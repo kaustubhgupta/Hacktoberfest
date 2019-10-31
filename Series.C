41d5a606aed8e2cbc4fa44dd49041c84e9983c3f
anmolsingla <57210856+anmolsingla@users.noreply.github.com>
2019-10-31 12:44:28 +0530

    updates in series
    syntax errors

--- Series.C
+++ Series.C
@@ -2,10 +2,10 @@
 #include<conio.h>
 void main()
 {
-  int a
-  printf("Enter value upto which you want series:")
+  int a;
+  printf("Enter value upto which you want series:");
   scanf("%d", &a);
-  for( int i=1,i<=a;i)
+  for( int i=1,i<=a;i++)
   {
     printf("\t%d",i);
     
