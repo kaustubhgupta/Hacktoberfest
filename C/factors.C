d581e797f15090d95e976e6f362ae69a10f7762a
Ashis Kumar Singh <49238464+ashiscs@users.noreply.github.com>
2020-10-01 01:29:31 +0530

    Update factors.C

--- C/factors.C
+++ C/factors.C
@@ -2,15 +2,14 @@
 #include<conio.h>
 void main()
 {
-  int a b;
+  int a,b;
   pritf("Enter a no:");
   scan("%d", &a);
   for(int i=1;i<=a;i++)
   {
     if(a%i=0)
     b=a/i;
-    printf("%d\n",b)
-
+    printf("%d\n",b);
   }
    getch();
-   }
+  }
