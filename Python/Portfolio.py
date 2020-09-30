eb1ddce9f67bf00c4486ce7cbbcf21acdf6d42cd
Davis Schenkenberger <35881002+davisschenk@users.noreply.github.com>
2020-09-30 16:07:07 -0600

    Fixed styling to better align with PEP 8

--- Python/Portfolio.py
+++ Python/Portfolio.py
@@ -1,13 +1,15 @@
-name="Vandit"
-print("Name : ",name)
-branch="CSE"
-print("Branch : ",branch)
-Skills=['Python', 'C Programming', 'HTML']
+name = "Vandit"
+branch= "CSE"
+skills = ["Python", "C Programming", "HTML"]
+hobbies = ["Watching American TV Shows", "Playing Guitar", "Singing"]
+
+print(f"Name: {name}")
+print(f"Branch : {branch}")
 print("Skills : ")
-for skill in Skills:
-  print("   ",skill)
-Hobbies=['Watching American TV Shows', 'Playing Guitar', 'Singing']
+for skill in skills:
+  print(f"\t{skill}")
+  
 print("Hobbies : ")
-for hobby in Hobbies:
-  print("   ",hobby)
+for hobby in hobbies:
+  print(f"\t{hobby}")
 print("-------Thank You-------")
