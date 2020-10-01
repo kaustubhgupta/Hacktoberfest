d043211e10ad149a596e6119d63aeb64d0d3cdcc
Programmer <55909772+YashMirge@users.noreply.github.com>
2020-10-01 12:46:05 +0530

    Updated

--- C/days of month.C
+++ C/days of month.C
@@ -1,28 +1,29 @@
 #include<stdio.h>
 #include<conio.h>
-void main()
+
+int main()
 {
-   int a,y;
-   printf("\nEnter any month no:");
-   scanf("%d", &a);
-   if(a==1||a==3||a==5||a=7||a==8||a==10||a==12)
+   int month,year;
+   printf("\n Enter any month no:");
+   scanf("%d", &month);
+   if(month==1||month==3||month==5||month=7||month==8||month==10||month==12)
    {
      printf("It has 31 days");
-     }
+   }
    else
    {
-     if(a==4||a==6||a==9||a==11)
+     if(month==4||month==6||month==9||month==11)
      {
        printf("It has 30 days");
-       }
+     }
      else
      {
-	if(a=2)
+	if(a==2)
 	{
 	   printf("Enter year:");
-	   scanf("%d", &y);
+	   scanf("%d", &year);
 
-	   if(y%4=0)
+	   if(y%4==0)
 	   {
 	    printf("It has 29 days");
 	   }
@@ -31,13 +32,13 @@ void main()
 	     printf("It has 28 days");
 
 	   }
-
-
 	}
 	else
 	{
-	 printf("This is not valid input");
+	 printf("This is not a valid input");
 	}
+     }
+   }
 
-   getch();
+   return 0;
    }
