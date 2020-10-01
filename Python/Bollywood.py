5c004d11c0a2a99dc3a409810bd983b767f4e3c0
Sattwik Palai <sattwikpalai21@gmail.com>
2020-10-01 10:13:34 +0530

    Update Bollywood.py

--- Python/Bollywood.py
+++ Python/Bollywood.py
@@ -1,8 +1,10 @@
 import random
-movies = ('LAGAAN', 'HIGHWAY', 'THOR', 'DANGAL', 'NEWTON','ANEK','APNE','CHHICHHORE','WAR','GULAAL','DHOOM','MALANG', 'STAR WARS')
-c_m = random.sample(movies,1)
+movies = ('LAGAAN', 'HIGHWAY', 'THOR', 'DANGAL', 'NEWTON','ANEK','APNE','CHHICHHORE','WAR','GULAAL','DHOOM','MALANG', 'STAR WARS') #movies_name_that_you_will_get_to_play
+c_m = random.sample(movies,1)    #one movie is selected by random library
 k = '*' * len(c_m[0])
-print(k)
+print(k) #output movie name in stars
+
+
 lap = {}
 for i in range(len(c_m[0])):
     if c_m[0][i] not in lap.keys():
@@ -14,21 +16,23 @@ for i in range(len(c_m[0])):
         j.append(i)
         lap[c_m[0][i]] = j
 score = 10
-print(f'Score = {score}')
+
+print(f'Score = {score}')                              #prints the starting score
+
 while score > 0 and '*' in k:
-    letter = input("Guess a letter:").capitalize()
-    if letter in lap.keys():
-        n = lap[letter]
+    letter = input("Guess a letter:").capitalize()     #capitalizes the user input
+    if letter in lap.keys():                           # checks if the letter is present in the movie name  
+        n = lap[letter]  
         for p in n:
             k = k[:p]+letter+k[p+1:]
-        score = score+3
+        score = score+3                                #increases the points ofg the player
         print('correct guess')
         print(k)
         print(f' Score= {score}')
         lap.pop(letter)
+#correct guess adds three points
 
-
-    else:
+    else:                                               #deducts the points if the letter is not present in movie name
         score = score-2
         print('incorrect guess')
         print(k)
@@ -40,4 +44,4 @@ if '*' not in k:
 else:
     print("YOU LOOSE")
 
-print(f"The Answer was {c_m[0]}")
+print(f"The Answer was {c_m[0]}")                     #prints the correct answer
