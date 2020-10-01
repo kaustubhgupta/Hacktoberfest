05a63857778c650b30ec7d99c271fcb48e56e226
Kabir Jain <kabirjain15@gmail.com>
2020-10-01 17:52:31 +0530

    Add files via upload

---
+++ Java/Java_evenodd_example.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+//This code tells whether a given number is even or odd
+public class Java_evenodd_example {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println(num % 2 == 0 ? "The number is even" : "The number is odd");
+    }
+}
