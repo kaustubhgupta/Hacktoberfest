8f2ab3eb3e5b0c87e8046038cc7a7d0d15ac77f6
Sulthan Mohaideen <32130871+SulthanNK@users.noreply.github.com>
2020-10-01 14:59:57 +0530

    Create days_in_month.py

---
+++ Python/days_in_month.py
@@ -0,0 +1,11 @@
+# Use list indexing to determine how many days are in a particular month based on the integer variable month, and store that value in the integer variable num_days. For example, if month is 8, num_days should be set to 31, since the eighth month, August, has 31 days.
+
+# Remember to account for zero-based indexing!
+
+month = 10
+days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+# use list indexing to determine the number of days in month
+num_days = days_in_month[month -1]
+
+print(num_days)
