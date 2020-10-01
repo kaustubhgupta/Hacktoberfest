3a59dd38a9f03ffc578db7b396ede32b15cedbec
Programmer <55909772+YashMirge@users.noreply.github.com>
2020-10-01 07:55:27 +0530

    Update

--- C/days of month.C
+++ C/days of month.C
@@ -2,8 +2,8 @@
 #include<conio.h>
 void main()
 {
-   int a,y
-   printf"\nEnter any month no:");
+   int a,y;
+   printf("\nEnter any month no:");
    scanf("%d", &a);
    if(a==1||a==3||a==5||a=7||a==8||a==10||a==12)
    {
