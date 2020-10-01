3c4538a87ee9350c0f59be0247af56cdea2cc7a1
raksharaj1122 <72208856+raksharaj1122@users.noreply.github.com>
2020-10-01 19:23:03 +0530

    Update sum of digits of a number.c

--- C/sum of digits of a number.C
+++ C/sum of digits of a number.C
@@ -2,9 +2,9 @@
 void main()
 {
   int a,b,sum=0;
-  printf("Enter a no:");
+  printf("Enter a no:\n");
   scanf("%d", &a);
-  printf("Enter another no:");
+  printf("Enter another no:\n");
   scanf("%d", &b);
   sum=a+b;
   printf("Sum of digits is %d",sum);
