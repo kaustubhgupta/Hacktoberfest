ae9b764b3d18a555a69b76cb081b3db4de9b89b0
Laura de Oliveira <laura@pmweb.com.br>
2020-10-01 19:52:26 -0300

    Add API REST with Node and Express

---
+++ Others/API-REST-Node/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const data = require('./data.json');
+
+app.use(express.json());
+
+app.get("/clients", function(req, res) {
+    
+    res.json(data);
+
+});
+app.get("/clients/:id", function(req, res) {
+    
+    const { id } = req.params;
+    const client = data.find(cli => cli.id == id);
+    
+    if (!client)
+        return res.status(204).json();
+
+    res.json(client);
+
+});
+app.post("/clients", function(req, res) {
+
+    const { name, phone } = req.body;
+
+    // persistence logic here
+
+    res.json({ name, phone });
+
+});
+app.put("/clients/:id", function(req, res) {
+
+    const { id } = req.params;
+    const client = data.find(cli => cli.id == id);
+
+    if (!client) return res.status(204).json();
+
+    
+    // Update client name
+    const { name } = req.body;
+
+    client.name = name;
+
+    res.json(client);
+
+});
+app.delete("/clients/:id", function(req, res) {
+
+    const { id } = req.params;
+    const clientFiltered = data.filter(cli => cli.id != id);
+
+    res.json(clientFiltered);
+
+});
+
+app.listen(3000, function() {
+    console.log("Hey! Ready for your endpoints test!");
+});
