32bfcf3924a7e06e8c4e4e7b188558ec8eb1496d
Sattwik Palai <sattwikpalai21@gmail.com>
2020-10-02 14:58:38 +0530

    changed k into starred_movie

--- Python/Bollywood.py
+++ Python/Bollywood.py
@@ -1,8 +1,8 @@
 import random
 movies = ('LAGAAN', 'HIGHWAY', 'THOR', 'DANGAL', 'NEWTON','ANEK','APNE','CHHICHHORE','WAR','GULAAL','DHOOM','MALANG', 'STAR WARS') #movies_name_that_you_will_get_to_play
 selected_movie = random.sample(movies,1)    #one movie is selected by random library
-k = '*' * len(selected_movie[0])
-print(k) #output movie name in stars
+starred_movie = '*' * len(selected_movie[0])
+print(starred_movie) #output movie name in stars
 
 
 lap = {}                                         #assigning the stars values the letters of movie
@@ -19,15 +19,15 @@ score = 10
 
 print(f'Score = {score}')                              #prints the starting score
 
-while score > 0 and '*' in k:
+while score > 0 and '*' in starred_movie:
     letter = input("Guess a letter:").capitalize()     #capitalizes the user input
     if letter in lap.keys():                           # checks if the letter is present in the movie name  
         n = lap[letter]  
         for p in n:
-            k = k[:p]+letter+k[p+1:]
+            starred_movie = starred_movie[:p]+letter+starred_movie[p+1:]
         score = score+3                                #increases the points ofg the player
         print('correct guess')
-        print(k)
+        print(starred_movie)
         print(f' Score= {score}')
         lap.pop(letter)
 #correct guess adds three points
@@ -35,10 +35,10 @@ while score > 0 and '*' in k:
     else:                                               #deducts the points if the letter is not present in movie name
         score = score-2
         print('incorrect guess')
-        print(k)
+        print(starred_movie)
         print(f'Score={score}')
 
-if '*' not in k:
+if '*' not in starred_movie:
     print("YOU WON ")
 
 else:
