05eb5f3f4b94039525508a49b7e3c58ac1c20494
akshaykrad <58693817+akshaykrad@users.noreply.github.com>
2020-10-02 20:25:17 +0530

    Update MatrixMultiplication.cpp

--- C++/MatrixMultiplication.cpp
+++ C++/MatrixMultiplication.cpp
@@ -7,9 +7,10 @@ int main()
 int a[10][10],b[10][10],mul[10][10],r,c,i,j,k;
 cout<<"enter the number of row=";
 cin>>r;
-cout<<"enter the number of column=";
+// Taking in the first matrix.
+cout<<"Enter the number of column=";
 cin>>c;
-cout<<"enter the first matrix element=\n";
+cout<<"Enter the first matrix elements=\n";
 for(i=0;i<r;i++)
 {
 for(j=0;j<c;j++)
@@ -17,6 +18,7 @@ for(j=0;j<c;j++)
 cin>>a[i][j];
 }
 }
+//Taking in the second matrix.
 cout<<"enter the second matrix element=\n";
 for(i=0;i<r;i++)
 {
@@ -25,6 +27,7 @@ for(j=0;j<c;j++)
 cin>>b[i][j];
 }
 }
+// Multiplying the two matrices.
 cout<<"multiply of the matrix=\n";
 for(i=0;i<r;i++)
 {
@@ -37,7 +40,7 @@ mul[i][j]+=a[i][k]*b[k][j];
 }
 }
 }
-//for printing result
+//Printing out the resultant matrix.
 for(i=0;i<r;i++)
 {
 for(j=0;j<c;j++)
