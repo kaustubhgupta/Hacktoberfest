4c2922f57cfa3a0dc1042becc7f668c707bc08c8
navneetofficals <72147434+navneetofficals@users.noreply.github.com>
2020-10-03 20:23:23 +0530

    Create whileloopJS.js

---
+++ Others/whileloopJS.js
@@ -0,0 +1,24 @@
+<html>
+
+      <head>
+
+      </head>
+         
+      <body>
+
+              <h1>Welcome JS Hackers</h1>
+            
+              <script> 
+                        var counter=100;
+                        while(counter > 0)
+                        {
+                                          document.write("<p>Counter Value is : " + counter + "</p>");
+                                          counter--;
+                        }   
+
+
+              </script>
+
+      </body>
+
+</html>
