dd8cc8d6b9f41beb92723311b7f5f1bf83a0964a
Nishchay <56715777+codernishchay@users.noreply.github.com>
2020-10-05 21:09:24 +0530

    Updated changes in Binary search tree

--- Java/binarySearchTree.java
+++ Java/binarySearchTree.java
@@ -1,3 +1,6 @@
+import java.lang.Math; 
+  
+
 class BST_class { 
     //node class that defines BST node
     class Node { 
@@ -114,44 +117,19 @@ class BST_class {
         return search_Recursive(root.right, key); 
     } 
 }
-class Main{
+public class Main{
     public static void main(String[] args)  { 
        //create a BST object
         BST_class bst = new BST_class(); 
-        /* BST tree example
-              45 
-           /     \ 
-          10      90 
-         /  \    /   
-        7   12  50   */
-        //insert data into BST
-        bst.insert(45); 
-        bst.insert(10); 
-        bst.insert(7); 
-        bst.insert(12); 
-        bst.insert(90); 
-        bst.insert(50); 
-        //print the BST
-        System.out.println("The BST Created with input data(Left-root-right):"); 
-        bst.inorder(); 
         
-        //delete leaf node  
-        System.out.println("\nThe BST after Delete 12(leaf node):"); 
-        bst.deleteKey(12); 
+        for(int i=0; i<10; i++){
+        int rand = (int)(Math.random() * 10) + 1; 
+        bst.insert(rand); 
+        }
         bst.inorder(); 
-        //delete the node with one child
-        System.out.println("\nThe BST after Delete 90 (node with 1 child):"); 
-        bst.deleteKey(90); 
-        bst.inorder(); 
-                 
-        //delete node with two children  
-        System.out.println("\nThe BST after Delete 45 (Node with two children):"); 
-        bst.deleteKey(45); 
-        bst.inorder(); 
-        //search a key in the BST
-        boolean ret_val = bst.search (50);
-        System.out.println("\nKey 50 found in BST:" + ret_val );
-        ret_val = bst.search (12);
-        System.out.println("\nKey 12 found in BST:" + ret_val );
-     } 
+         //randomly deleting any number.. 
+        if(bst.search(10)){
+        bst.deleteKey(10); 
+        }
+    } 
 }
