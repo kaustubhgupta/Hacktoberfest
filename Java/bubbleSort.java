2c985c3a270915e02f0d16ba60cbbc0caa5da292
Manvi Gupta <0102manvigupta@gmail.com>
2020-10-06 19:02:30 +0530

    Create bubbleSort.java

---
+++ Java/bubbleSort.java
@@ -0,0 +1,56 @@
+package ARRAY;
+
+import java.util.Scanner;
+
+public class BUBBLESORT {
+
+	static Scanner scn = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		int[] array = takeInput();
+    
+		display(array);
+    
+    bubbleSort(arr);
+    
+    display(arr);
+    
+    }
+    
+    public static int[] takeInput() {
+		
+		System.out.println(" size? ");
+		int n = scn.nextInt();
+		
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println("enter the value for " + i + " index");
+			arr[i] = scn.nextInt();
+		}
+		return arr;
+	}
+	
+	public static void display(int[] arr) {
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+  
+  public static void bubbleSort(int[] arr) {
+		
+		for(int counter = 0; counter < arr.length-1; counter++) {
+			for(int j = 0; j < arr.length-1-counter; j ++) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+//					Swap(arr[j],arr[j+1]);
+				}
+			}
+		}
+	}
+  
+}
