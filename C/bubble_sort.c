9c46f28e78c0c7c90b9257691ff9df309bbe6c67
Yatindra29 <53404890+Yatindra29@users.noreply.github.com>
2020-10-06 18:00:36 +0530

    Updated bubble_sort.c
    Updated bubble_sort.c with comments

--- C/bubble_sort.c
+++ C/bubble_sort.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 
 int main(){
-int N;
+int N; //Number of elements in the array
 printf("\n Enter the number of elements in the array\n");
 scanf("%d",&N);
-int *arr=malloc(N*sizeof(*arr));
+int *arr=malloc(N*sizeof(*arr)); //Dynamic creation of an Integer array of Size N
 printf("\n Enter the elemnts of the array\n");
 for(int i=0;i<N;i++){
-  scanf("%d",&arr[i]);
+  scanf("%d",&arr[i]); //Inputting the elements of array
 }
-printf("\n Original Array\n");
+printf("\n Original Array\n"); //Printing the original array
 for(int i=0;i<N;i++){
   printf("%d ",arr[i]);
 }
-int temp;
-for(int i=0;i<N-1;i++){
+int temp; //Temporary variable used for swapping later
+for(int i=0;i<N-1;i++){ //Sorting process begins
   for(int j=0;j<N-i-1;j++){
-    if(arr[j]>arr[j+1]){
+    if(arr[j]>arr[j+1]){ //Checking if a number at the previous index is greater than the number at its next,if so, swapping takes place
       temp=arr[j];
       arr[j]=arr[j+1];
       arr[j+1]=temp;
     }
   }
 }
-printf("\n Sorted Array\n");
+printf("\n Sorted Array\n"); //Printing sorted array
 for(int i=0;i<N;i++){
   printf("%d ",arr[i]);
 }
