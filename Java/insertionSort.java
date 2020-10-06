0ce538fc38096eba33409efbfa0efddceaedc2ff
Manvi Gupta <0102manvigupta@gmail.com>
2020-10-06 20:16:53 +0530

    fixes (#197) (#217)

---
+++ Java/insertionSort.java
@@ -0,0 +1,59 @@
+package ARRAY;
+
+import java.util.Scanner;
+
+public class INSERTIONSORT {
+
+	static Scanner scn = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		int[] array = takeInput();
+
+		display(array);
+
+    insertionSort(arr);
+
+    display(arr);
+
+    }
+
+    public static int[] takeInput() {
+
+		System.out.println(" size? ");
+		int n = scn.nextInt();
+
+		int[] arr = new int[n];
+
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println("enter the value for " + i + " index");
+			arr[i] = scn.nextInt();
+		}
+		return arr;
+	}
+
+	public static void display(int[] arr) {
+
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+public static void insertionSort(int[] arr) {
+		
+		for(int counter = 1; counter <= arr.length - 1; counter++) {
+			
+			int val = arr[counter];
+			
+			int j = counter - 1;
+			
+			while(j >= 0 && arr[j] > val) {
+				arr[j+1] = arr[j];
+				j--;
+			}
+			
+			arr[j+1] = val;
+		}
+	}
+
+}
