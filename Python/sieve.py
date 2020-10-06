26762500bed8365b947bd245fb669f01715eeb85
Siddhant N <71310931+yatocodes@users.noreply.github.com>
2020-10-06 09:00:58 +0530

    Seive Of  Erasthosnes.
    Seive Of  Erasthosnes; Made In Python.

---
+++ Python/sieve.py
@@ -0,0 +1,28 @@
+#Sieve Of Eratosthenes
+
+'''
+About - The programme is created for the purpose of calculating
+prime numbers; between two of the user given numbers.
+'''
+
+print("Welcome To Sieve Of Eratosthenes \n")
+print("Plese Dont't Enter 1 as Starting Number\n")
+
+starting_num = int(input("Enter the starting number :  "))
+ending_num = int(input("Enter the ending number :  "))
+
+for numbers in range(starting_num, ending_num+1):
+    if(starting_num>1):
+        for i in range(2,numbers):
+            if(numbers%i)==0:
+                break
+        else:
+            print(numbers)
+            
+
+#Coded By - Siddhant aka "Jack"    
+
+
+       
+
+
