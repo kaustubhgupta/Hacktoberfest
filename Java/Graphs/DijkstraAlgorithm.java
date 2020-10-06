748a18264b1f1eeb24201f0f60af813b2dff829a
Deepanshu Mourya <deepanshumourya17@gmail.com>
2020-10-06 15:19:21 +0530

    Solved the issue to pass all checks for PR #213

--- Java/Graphs/DijkstraAlgorithm.java
+++ Java/Graphs/DijkstraAlgorithm.java
@@ -16,51 +16,50 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
-class Edge {
-    int source, dest, weight;
-
-    public Edge(int source, int dest, int weight) {
-        this.source = source;
-        this.dest = dest;
-        this.weight = weight;
-    }
-}
-
-// Data structure to store heap nodes
-class Node {
-    int vertex, weight;
-
-    public Node(int vertex, int weight) {
-        this.vertex = vertex;
-        this.weight = weight;
-    }
-}
-
-// class to represent a graph object
-class Graph {
-	
-    // A List of Lists to represent an adjacency list
-    List<List<Edge>> adjList = null;
-
-    // Constructor
-    Graph(List<Edge> edges, int N)
-    {
-        adjList = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
-            adjList.add(new ArrayList<>());
-        }
-
-        // add edges to the undirected graph
-        for (Edge edge: edges) {
-            adjList.get(edge.source).add(edge);
-        }
-    }
-}
-
 public class DijkstraAlgorithm {
-    private static void getRoute(int[] prev, int i, List<Integer> route)
-	{
+	static class Edge {
+		int source, dest, weight;
+	
+		public Edge(int source, int dest, int weight) {
+			this.source = source;
+			this.dest = dest;
+			this.weight = weight;
+		}
+	}
+	
+	// Data structure to store heap nodes
+	static class Node {
+		int vertex, weight;
+	
+		public Node(int vertex, int weight) {
+			this.vertex = vertex;
+			this.weight = weight;
+		}
+	}
+	
+	// class to represent a graph object
+	static class Graph {
+		
+		// A List of Lists to represent an adjacency list
+		List<List<Edge>> adjList = null;
+	
+		// Constructor
+		Graph(List<Edge> edges, int N)
+		{
+			adjList = new ArrayList<>();
+	
+			for (int i = 0; i < N; i++) {
+				adjList.add(new ArrayList<>());
+			}
+	
+			// add edges to the undirected graph
+			for (Edge edge: edges) {
+				adjList.get(edge.source).add(edge);
+			}
+		}
+	}
+
+    private static void getRoute(int[] prev, int i, List<Integer> route) {
 		if (i >= 0) {
 			getRoute(prev, prev[i], route);
 			route.add(i);
@@ -68,8 +67,7 @@ public class DijkstraAlgorithm {
 	}
 
 	// Run Dijkstra's algorithm on given graph
-	public static void shortestPath(Graph graph, int source, int N)
-	{
+	public static void shortestPath(Graph graph, int source, int N) {
 		// create min heap and push source node having distance 0
 		PriorityQueue<Node> minHeap;
 		minHeap = new PriorityQueue<>(Comparator.comparingInt(node -> node.weight));
