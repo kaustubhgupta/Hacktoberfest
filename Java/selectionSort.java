8e2470ffbb9afe8fe72534325a68f41b12a367c7
MohammadMasoomiH <mohammadmasoomi2@users.noreply.github.com>
2020-10-07 12:33:11 +0330

    Lil refactoring. ;D
    Useing Wrappers instead of primitives!
    Using modern for instead of typical for!

--- Java/selectionSort.java
+++ Java/selectionSort.java
@@ -3,56 +3,48 @@ package ARRAY;
 import java.util.Scanner;
 
 public class SELECTIONSORT {
+    static Scanner scn = new Scanner(System.in);
 
-	static Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-
-		int[] array = takeInput();
-
-		display(array);
-
-    selectionSort(arr);
-
-    display(arr);
-
+        Integer[] array = takeInput();
+        display(array);
+        selectionSort(array);
+        display(array);
     }
 
-    public static int[] takeInput() {
+    public static Integer[] takeInput() {
 
-		System.out.println(" size? ");
-		int n = scn.nextInt();
+        System.out.println(" Insert size: ");
+        Integer n = scn.nextInt();
 
-		int[] arr = new int[n];
+        Integer[] arr = new Integer[n];
+        for(Integer i = 0; i < arr.length; i++) {
+            System.out.println("enter the value for " + i + " index");
+            arr[i] = scn.nextInt();
+        }
+        return arr;
+    }
 
-		for(int i = 0; i < arr.length; i++) {
-			System.out.println("enter the value for " + i + " index");
-			arr[i] = scn.nextInt();
-		}
-		return arr;
-	}
+    public static void display(Integer[] arr) {
 
-	public static void display(int[] arr) {
+        for(Integer element : arr) {
+            System.out.println(element);
+        }
+    }
 
-		for(int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
-		}
-	}
+    public static void selectionSort(Integer[] arr) {
 
-public static void selectionSort(int[] arr) {
-		
-		for(int counter = 0; counter < arr.length-1; counter++) {
-			int min = counter;
-			for(int j = counter + 1;j < arr.length; j++) {
-				if(arr[j] < arr[min]) {
-					min = j;
-				}
-			int temp = arr[min];
-			arr[min] = arr[counter];
-			arr[counter] = temp;
-				
-			}
-		}
-	}
-
+        for(Integer counter = 0; counter < arr.length-1; counter++) {
+            Integer min = counter;
+            for(Integer j = counter + 1; j < arr.length; j++) {
+                if(arr[j] < arr[min]) {
+                    min = j;
+                }
+                Integer temp = arr[min];
+                arr[min] = arr[counter];
+                arr[counter] = temp;
+            }
+        }
+    }
 }
