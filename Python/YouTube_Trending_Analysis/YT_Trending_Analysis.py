3745a4c5c0c6ac1b8e33c84f51665b95fe68ce2a
Adit Sawant <aditsawant@gmail.com>
2020-10-07 23:49:57 +0530

    Update YT_Trending_Analysis.py

--- Python/YouTube_Trending_Analysis/YT_Trending_Analysis.py
+++ Python/YouTube_Trending_Analysis/YT_Trending_Analysis.py
@@ -2,11 +2,15 @@ import json
 import time
 import pandas as pd
 from selenium import webdriver
+# driver is our way to access the webpages
+# This code is meant to be run using the Google Chrome browser. Any other browser would require you to change the following function. 
+# eg. webdriver.Firefox for the Firefox browser.
 driver = webdriver.Chrome()
 driver.implicitly_wait(5)
 baseUrl = "https://www.youtube.com/feed/trending"
 all_details = []
 
+# This returns a list of links to all the trending videos
 def getVideoURLs():
     driver.get(baseUrl)
     time.sleep(3)
@@ -14,6 +18,7 @@ def getVideoURLs():
     links = list(dict.fromkeys(map(lambda a: a.get_attribute("href"),allVideoList)))
     return links
 
+# This returns the specified details about the input video
 def getVideoDetails(video):
 	driver.get(video)
 	time.sleep(1.5)
@@ -33,6 +38,8 @@ if __name__ == '__main__':
 	videos = getVideoURLs()
 	for video in videos:
 		all_details.append(getVideoDetails(video))
-
+	
+	# all_details is a list of lists which contain the video details
+	# Making a DataFrame to extract the data in a CSV
 	df = pd.DataFrame(all_details, columns = ["Title", "Views", "Likes", "Dislikes"])
 	df.to_csv('Trending_Page_Data.csv')
