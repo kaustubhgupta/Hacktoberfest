a062757476a686daed43be9156483cc2840d3e09
shubhamc1200 <65861993+shubhamc1200@users.noreply.github.com>
2020-10-07 18:20:42 +0530

    Update LinearSearch.java

--- Java/LinearSearch.java
+++ Java/LinearSearch.java
@@ -8,7 +8,7 @@ public static int linearSearch(int[] arr, int key){
         }    
         return -1;    
     }    
-    public static void main(String a[]){    
+    public static void main(String args[]){    
         int[] a1= {10,20,30,50,70,90};    
         int key = 50;    
         System.out.println(key+" is found at index: "+linearSearch(a1, key));    
