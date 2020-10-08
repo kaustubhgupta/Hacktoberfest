79b1bfa9ee32eba8a403955672bc666df68d8c38
AnOnYmOus001100 <47621801+AnOnYmOus001100@users.noreply.github.com>
2020-10-08 14:07:22 +0530

    added swapTwoNumbers.java

---
+++ Java/swapTwoNumbers.java
@@ -0,0 +1,41 @@
+/**
+	Author: AnOnYmOus001100
+	Date: 04/10/2020
+
+	Description: swap the value of two numbers without using temporary variable
+*/
+
+import java.util.Scanner;
+
+public class swapTwoNumbers {
+
+	// swap method, to swap the values, takes two integer numbers as parameter
+	public static void swap(int num1, int num2)
+	{
+		System.out.println("Before swapping: " + num1 + " " + num2);
+
+		// swapping
+		num1 = num1 + num2;
+		num2 = num1 - num2;
+		num1 = num1 - num2;
+
+		System.out.println("After swapping: " + num1 + " " + num2);
+	}
+
+
+	// main method
+	public static void main(String[] args)
+	{
+		int num1, num2;
+		Scanner sc = new Scanner(System.in);
+		
+		// input prompt
+		System.out.println("Enter two numbers: ");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+
+		// calling swap method, num1 and num2 as arguments
+		swap(num1,num2);
+	}
+}
+
