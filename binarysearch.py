70589add3dd69c3c1bc9515d4056789b49c9882a
Blackwidow2981 <61281804+Blackwidow2981@users.noreply.github.com>
2020-10-08 20:37:41 +0530

    BinarySearch_Python

--- binarysearch.py
+++ binarysearch.py
@@ -16,16 +16,16 @@ def search (arr,l,r,x):
 
 
 #Accepting number of terms in an array
-num = input("Enter Number of terms:");
+num = input("Enter Number of terms:")
 #Since input() accepts in string datatype we convert it to int
 num=int(num)
-arr=[0]*num;
+arr=[0]*num
 #accept array elements
 print("Enter array elements")
 for i in range(0,num):
-    arr[i]=input();
+    arr[i]=input()
 #Enter number you want to search 
-x=input("number to search:");
+x=input("number to search:")
 #pass the array,the number of terms and the number to be searched to the Search function
 result = search (arr,0,num-1,x)
 if(result==-1):
