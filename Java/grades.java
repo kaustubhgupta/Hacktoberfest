ef1b68190f15743e4143381fd85a0a69cec46a8b
Abhishek Pandey <72411579+kn0wabhi@users.noreply.github.com>
2020-10-08 15:42:49 +0530

    Update grades.java
    Java Program to demonstrate the use of If else-if ladder.  
    •	 program of grading system for fail (<50), 
    •	D grade -marks>=50 && marks<60
    •	C grade- marks>=60 && marks<70,
    •	B grade- marks>=70 && marks<80 , 
    •	A grade- marks>=80 && marks<90 
    •	and A+ - marks>=90 && marks<100

--- Java/grades.java
+++ Java/grades.java
@@ -1,50 +1,78 @@
-/*Java Program to demonstrate the use of If else-if ladder.
-• program of grading system for fail (&lt;50),
-• D grade -marks&gt;=50 &amp;&amp; marks&lt;60
-• C grade- marks&gt;=60 &amp;&amp; marks&lt;70,
-• B grade- marks&gt;=70 &amp;&amp; marks&lt;80 ,
-• A grade- marks&gt;=80 &amp;&amp; marks&lt;90
-• and A+ - marks&gt;=90 &amp;&amp; marks&lt;100.*/
 import java.util.Scanner;
-public class Grade
+public class grades
+
 {
+
 public static void main(String args[])
+
 {
+
 System.out.println(&quot;Enter Marks: &quot;);
 ABC S=new ABC();
 S.display();
+
 }
+
 }
+
 class ABC
+
 {
+
 Scanner A = new Scanner(System.in);
 int marks=A.nextInt();
 public void display()
+
 {
-if(marks&lt;50)
+
+if(marks&lt;50)                                   /* fail (&lt;50)*/
+
 {
+
 System.out.println(&quot;Fail&quot;);
+
 }
-else if(marks&gt;=50 &amp;&amp; marks&lt;60)
+
+else if(marks&gt;=50 &amp;&amp; marks&lt;60)      /* D grade -marks&gt;=50 &amp;&amp; marks&lt;60*/
+
 {
+
 System.out.println(&quot;D Grade&quot;);
+
 }
-else if(marks&gt;=60 &amp;&amp; marks&lt;70)
+
+else if(marks&gt;=60 &amp;&amp; marks&lt;70)      /* C grade- marks&gt;=60 &amp;&amp; marks&lt;70*/
+
 {
+
 System.out.println(&quot;C Grade&quot;);
+
 }
-else if(marks&gt;=70 &amp;&amp; marks&lt;80)
+
+else if(marks&gt;=70 &amp;&amp; marks&lt;80)      /* B grade- marks&gt;=70 &amp;&amp; marks&lt;80*/
+
 {
+
 System.out.println(&quot;B Grade&quot;);
+
 }
-else if(marks&gt;=80 &amp;&amp; marks&lt;90)
+
+else if(marks&gt;=80 &amp;&amp; marks&lt;90)      /* A grade- marks&gt;=80 &amp;&amp; marks&lt;90*/
+
 {
+
 System.out.println(&quot;A Grade&quot;);
+
 }
+
 else
 
 {
-System.out.println(&quot;A+ Grade&quot;);
+
+System.out.println(&quot;A+ Grade&quot;);         /* A+ - marks&gt;=90 &amp;&amp; marks&lt;100*/
+
 }
+
 }
+
 }
