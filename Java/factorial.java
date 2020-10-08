a48bd727c63a5ef4d772660d1e955c731cb0cbe0
AnOnYmOus001100 <47621801+AnOnYmOus001100@users.noreply.github.com>
2020-10-08 14:06:47 +0530

    added factorial.java

---
+++ Java/factorial.java
@@ -0,0 +1,40 @@
+/**
+	Author: AnOnYmOus001100
+	Date: 04/10/2020
+
+	Description: inputs a number and displays the factorial of the number
+
+*/
+
+import java.util.Scanner;
+
+public class factorial {
+
+	// fact method to calculate factorial, takes integer n as parameter and returns the factorial
+	public static int fact(int n)
+	{
+		int factorial = 1;
+
+		// loop to calculate factorial
+		for (int i = n; i > 0; i--)
+			factorial *= i;
+
+		return factorial;
+	}
+
+	// main method
+	public static void main(String[] args)
+	{
+		int num, res;
+		Scanner sc = new Scanner(System.in);
+
+		// input number
+		System.out.println("Enter a number to find factorial: ");
+		num = sc.nextInt();
+
+		// calling fact method with num argument
+		res = fact(num);
+		System.out.println("The factorial of "+ num + " is " + res);
+	}
+	
+}
