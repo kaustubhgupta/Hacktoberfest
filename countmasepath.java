1055f567df82d60dd50d85be10969e6d6c09a9bc
Coding Point <72379281+Coding-Point@users.noreply.github.com>
2020-10-08 18:14:13 +0530

    Update countmasepath.java

--- countmasepath.java
+++ countmasepath.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class countmasepath {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
 
