d4772286ec992d0f111227751c35a797bab842aa
13snehitha <domakuntlasnehitha35@gmail.com>
2020-10-09 17:38:22 +0530

    First Commit

---
+++ Python/kadanes_algo.py
@@ -0,0 +1,24 @@
+ip=list(map(int,input("Enter array elements").split()))
+i,j,summ,maxsum=0,0,0,0
+while(i<len(ip)-1 and ip[i]<0):
+    i=i+1
+summ=summ+ip[i]
+j=i+1
+while( j<len(ip)):
+    if(ip[j]>=0):
+        summ=summ+ip[j]
+        j=j+1
+    else:
+        if(summ>maxsum):
+            maxsum=summ
+        i=j
+        while(i<len(ip)-1 and ip[i]<0):
+            i=i+1
+        summ=ip[i]
+        j=i+1
+if(summ>maxsum):
+    maxsum=summ
+print("Maximum positive sum = ",maxsum)
+            
+    
+
