239332b9c86150c41b0421f67d3247c0ea65baee
Blackwidow2981 <61281804+Blackwidow2981@users.noreply.github.com>
2020-10-09 14:08:06 +0530

    Changes

--- Python/binarysearch.py
+++ Python/binarysearch.py
@@ -1,4 +1,4 @@
-#Function to check if the number exists ,the array is sorted
+  # Function to check if the number exists ,the array is sorted
 def search (arr,l,r,x):
     if l > r:
         return -1
@@ -11,17 +11,17 @@ def search (arr,l,r,x):
         
     
 
-num = input("Enter Number of terms:")#Accepting number of terms in an array
-num=int(num)#Since input() accepts in string datatype we convert it to int
+num = input("Enter Number of terms:")  # Accepting number of terms in an array
+num=int(num)  # Since input() accepts in string datatype we convert it to int
 arr=[0]*num
 
-print("Enter array elements")#accept array elements
+print("Enter array elements")  # accept array elements
 for i in range(0,num):
     arr[i]=input()
 
-x=input("number to search:")#Enter number you want to search 
+x=input("number to search:")  # Enter number you want to search 
 
-result = search (arr,0,num-1,x)#pass the array,the number of terms and the number to be searched to the Search function
+result = search (arr,0,num-1,x)  # pass the array,the number of terms and the number to be searched to the Search function
 if(result==-1):
     print("Number not found")
 else:
