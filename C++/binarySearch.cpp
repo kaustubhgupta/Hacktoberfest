e7bfe23bfd6d3f7f31adfb1adfc88fa45c02ebad
aparajita-paul <57225563+aparajita-paul@users.noreply.github.com>
2020-10-10 01:42:18 +0530

    Update binarySearch.cpp
    naming the target element as x can be misleading for beginners, so I changed it to target and also have changed a few formatting. another thing for calculating the value of the middle it is a better practice to do it like left + (right-left)/2

--- C++/binarySearch.cpp
+++ C++/binarySearch.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-//arr is array of integers, length is size of array, x is value to search
-int binarySearch(int arr[], int length, int x) {
+//arr is array of integers, length is size of array, target is value to search
+int binarySearch(int arr[], int length, int target) {
     //setting equal to index of first element
     int left = 0; 
 
@@ -13,30 +13,31 @@ int binarySearch(int arr[], int length, int x) {
     //approx average of left and right calculated during each iteration
     int middle;
 
-    while (left <= right) {
+    while (left <= right) //validating the search space
+    {
         //calculate middle
-        middle = (left + right) / 2;
+        middle = left + (right-left)/2; // same as middle=(left+right)/2, but (left+right) can overflow as middle is declared as int 
 
-        //if middle element is equal to x
-        if (arr[middle] == x) { 
+        //if middle element is equal to target
+        if (arr[middle] == target) { 
             //return middle(index)
             return middle; 
         } 
-        //if element is greater than middle change constraints, since element is greater than middle set left equal to middle and new middle is calculated accordingly
-        else if (x > arr[middle]) {
+        //if the target element is greater than middle change constraints, since element is greater than middle set left equal to middle and new middle is calculated accordingly
+        else if (target > arr[middle]) {
             left = middle + 1;
         }
-        //if element is smaller than middle change constraints, since element is smaller than middle set right equal to middle and new middle is calculated accordingly
+        //if the target element is smaller than middle change constraints, since element is smaller than middle set right equal to middle and new middle is calculated accordingly
         else {
             right = middle - 1;
         }
     }
 
-    return -1; //if not found return -1
+    return -1; //if the target is not found then return -1
 }
 
 int main() {
-    int arr[] = { 45, 98, 123, 147, 265, 308};
+    int arr[] = {45, 98, 123, 147, 265, 308};
 
     //test cases
     cout << binarySearch(arr, 6, 123)<<endl;
