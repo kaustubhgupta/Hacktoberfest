18c1cffd756ff09ad1c3eb1b86057c871edf732a
mmessmer <mmessmer@unibz.it>
2020-10-09 12:57:04 +0200

    Fix #316 Python String subset Recursive

--- Python/stringSubsetsRecursive.py
+++ Python/stringSubsetsRecursive.py
@@ -8,7 +8,10 @@ def strSubsetsRecursive(original, accumulator, subsets):
     if len(original) > 0:
         currentChar = original[0]
 
+        #go to the next char and add it to the accumulator
         strSubsetsRecursive(original[1:], accumulator + currentChar, subsets)
+        
+        #go to the next char and compute the subset without the accumulator
         strSubsetsRecursive(original[1:], accumulator, subsets)
         
         subsets.append(accumulator + currentChar)
