3d09476d76cc75db73623e008e77c5ae16962f81
13snehitha <domakuntlasnehitha35@gmail.com>
2020-10-10 21:37:32 +0530

    Commit

--- Python/kadanes_algorithm.py
+++ Python/kadanes_algorithm.py
@@ -1,9 +1,18 @@
+#give an array input
 ip=list(map(int,input().split()))
 i,j,summ,maxsum=0,0,0,0
+
+#check for the 1st positive number
 while(i<len(ip)-1 and ip[i]<0):
     i=i+1
+
+#add it to sum and traverse through the array
 summ=summ+ip[i]
 j=i+1
+
+#if encountered element is positive go on adding to sum
+#if negative element is encountered store the previous maximum sum
+#and start over from the next positive element
 while( j<len(ip)):
     if(ip[j]>=0):
         summ=summ+ip[j]
@@ -18,4 +27,6 @@ while( j<len(ip)):
         j=i+1
 if(summ>maxsum):
     maxsum=summ
+
+#print the maximum consecutive sum
 print(maxsum)
