a85544d1ccdc50bbfef2a4446399b2c2c346c6fc
Khushi Gangopadhyay <khushiganguly08@gmail.com>
2020-10-10 15:24:12 +0530

    Update mergesort.cpp

--- C++/mergesort.cpp
+++ C++/mergesort.cpp
@@ -1,42 +1,26 @@
-
 #include <bits/stdc++.h> 
 using namespace std; 
-
-
 class Node { 
 public: 
 	int data; 
 	Node* next; 
 }; 
-
-
 Node* SortedMerge(Node* a, Node* b); 
-void FrontBackSplit(Node* source, 
-					Node** frontRef, Node** backRef); 
-
-
+void FrontBackSplit(Node* source, Node** frontRef, Node** backRef); 
 void MergeSort(Node** headRef) 
 { 
 	Node* head = *headRef; 
 	Node* a; 
 	Node* b; 
-
-
 	if ((head == NULL) || (head->next == NULL)) { 
 		return; 
 	} 
-
-	
 	FrontBackSplit(head, &a, &b); 
-
-	
 	MergeSort(&a); 
 	MergeSort(&b); 
 	
 	*headRef = SortedMerge(a, b); 
 } 
-
-
 Node* SortedMerge(Node* a, Node* b) 
 { 
 	Node* result = NULL; 
@@ -46,8 +30,6 @@ Node* SortedMerge(Node* a, Node* b)
 		return (b); 
 	else if (b == NULL) 
 		return (a); 
-
-	
 	if (a->data <= b->data) { 
 		result = a; 
 		result->next = SortedMerge(a->next, b); 
@@ -58,17 +40,11 @@ Node* SortedMerge(Node* a, Node* b)
 	} 
 	return (result); 
 } 
-
-
-void FrontBackSplit(Node* source, 
-					Node** frontRef, Node** backRef) 
-{ 
-	Node* fast; 
+void FrontBackSplit(Node* source, Node** frontRef, Node** backRef) 
+{ 	Node* fast; 
 	Node* slow; 
 	slow = source; 
 	fast = source->next; 
-
-	
 	while (fast != NULL) { 
 		fast = fast->next; 
 		if (fast != NULL) { 
@@ -82,8 +58,6 @@ void FrontBackSplit(Node* source,
 	*backRef = slow->next; 
 	slow->next = NULL; 
 } 
-
-
 void printList(Node* node) 
 { 
 	while (node != NULL) { 
@@ -91,40 +65,29 @@ void printList(Node* node)
 		node = node->next; 
 	} 
 } 
-
-
 void push(Node** head_ref, int new_data) 
 { 
 
 	Node* new_node = new Node(); 
-
-
 	new_node->data = new_data; 
-
-
 	new_node->next = (*head_ref); 
 	(*head_ref) = new_node; 
 } 
 
 int main() 
 { 
-	
 	Node* res = NULL; 
 	Node* a = NULL; 
 	int x,size;
     cin>>size;
-
     for(int i=0; i<size; i++)
     {
         cin>>x;
         push(&a,x);
     }
-
 	MergeSort(&a); 
-
 	cout << "Sorted Linked List is: \n"; 
 	printList(a); 
-
 	return 0; 
 } 
 
