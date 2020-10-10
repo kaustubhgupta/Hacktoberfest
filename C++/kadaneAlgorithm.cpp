d97de6d04de1c1d17516e0d756af73c5931c51cf
Sarthak Arora <sarthakarora1503@gmail.com>
2020-10-10 17:39:21 +0530

    Kadane's Algorithm

---
+++ C++/kadaneAlgorithm.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// O(n) solution
+int Kadane(vector<int> a,int n) // n = size of vector a
+{
+    int sum=0,max=0,i;
+
+    int x = *max_element(a.begin(),a.end()); // function for finding the maximum element in a vector
+
+    // if largest element of the vector is a negative number then other
+    // elements of the vector are definitely negative
+    // therefore the answer is x only
+    if(x < 0)
+        return x; 
+
+    for(i = 0; i < n; i++)
+    {
+        sum += a[i];
+
+        // if sum is negative then sum will be taken as 0 
+        if(sum < 0)
+            sum = 0;
+
+        // else whenever the sum would be either positive or 0 then max is updated 
+        else
+        {
+            if(sum > max)
+                max = sum;
+        }
+        
+    }
+
+    return max;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector<int> a(n);
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+
+    cout<<Kadane(a,n);
+
+    return 0;
+}
