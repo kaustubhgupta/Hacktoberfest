ee2a7ea4561cc9d01268ab83a85b0ad1c4126784
sanidhyaj <60248420+sanidhyaj@users.noreply.github.com>
2020-10-11 12:42:12 +0530

    Update selection_sort.py

--- Python/Sorting_Algorithms/selection_sort.py
+++ Python/Sorting_Algorithms/selection_sort.py
@@ -1,23 +1,20 @@
-# Python program for implementation of Selection 
-# Sort 
-import sys 
-A = [64, 25, 12, 22, 11] 
-  
-# Traverse through all array elements 
-for i in range(len(A)): 
-      
-    # Find the minimum element in remaining  
-    # unsorted array 
-    min_idx = i 
-    for j in range(i+1, len(A)): 
-        if A[min_idx] > A[j]: 
-            min_idx = j 
-              
-    # Swap the found minimum element with  
-    # the first element         
-    A[i], A[min_idx] = A[min_idx], A[i] 
-  
-# Driver code to test above 
-print ("Sorted array") 
-for i in range(len(A)): 
-    print("%d" %A[i]),  
+def selectionSort( itemsList ):
+    n = len( itemsList )
+    for i in range( n - 1 ): 
+        minValueIndex = i
+
+        for j in range( i + 1, n ):
+            if itemsList[j] < itemsList[minValueIndex] :
+                minValueIndex = j
+
+        if minValueIndex != i :
+            temp = itemsList[i]
+            itemsList[i] = itemsList[minValueIndex]
+            itemsList[minValueIndex] = temp
+
+    return itemsList
+
+
+el = [21,6,9,33,3]
+
+print(selectionSort(el))  
