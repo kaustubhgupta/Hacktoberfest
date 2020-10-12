88e4a57eeafd629dff44a511dfe20fc23338c256
Manishita Choudhary <60927308+manishita24@users.noreply.github.com>
2020-10-12 20:42:35 +0530

    Update reverseDoublyLinkedList.java

--- Java/reverseDoublyLinkedList.java
+++ Java/reverseDoublyLinkedList.java
@@ -1,109 +1,86 @@
-// Java implementation to reverse  
-// a doubly linked list  
-public class GFG 
-{ 
-  
-// a node of the doubly linked list  
-static class Node  
-{  
-    int data;  
-    Node next, prev;  
-};  
-  
-// function to get a new node  
-static Node getNode(int data)  
-{  
-    // allocate space  
-    Node new_node = new Node();  
-  
-    // put in the data  
-    new_node.data = data;  
-    new_node.next = new_node.prev = null;  
-    return new_node;  
-}  
-  
-// function to insert a node at the beginging  
-// of the Doubly Linked List  
-static Node push(Node head_ref, Node new_node)  
-{  
-    // since we are adding at the beginning,  
-    // prev is always null  
-    new_node.prev = null;  
-  
-    // link the old list off the new node  
-    new_node.next = (head_ref);  
-  
-    // change prev of head node to new node  
-    if ((head_ref) != null)  
-        (head_ref).prev = new_node;  
-  
-    // move the head to point to the new node  
-    (head_ref) = new_node; 
-      
-    return head_ref; 
-}  
-  
-// function to reverse a doubly linked list  
-static Node reverseList(Node head_ref)  
-{  
-    // if list is empty or it contains  
-    // a single node only  
-    if ((head_ref) == null || ((head_ref).next) == null)  
-        return null;  
-  
-    Node new_head = null;  
-    Node curr = head_ref, next;  
-  
-    while (curr != null) 
-    {  
-  
-        // get pointer to next node  
-        next = curr.next;  
-  
-        // push 'curr' node at the beginning of the  
-        // list with starting with 'new_head'  
-        new_head = push(new_head, curr);  
-  
-        // update 'curr'  
-        curr = next;  
-    }  
-  
-    // update 'head_ref'  
-    head_ref = new_head; 
-      
-    return head_ref; 
-}  
-  
-// Function to print nodes in a  
-// given doubly linked list  
-static void printList(Node head)  
-{  
-    while (head != null)  
-    {  
-        System.out.print(head.data + " ");  
-        head = head.next;  
-    }  
-}  
-  
-// Driver program to test above  
-public static void main(String args[]) 
-{  
-    // Start with the empty list  
-    Node head = null;  
-  
-    // Create doubly linked: 10< - >8< - >4< - >2 /  
-    head = push(head, getNode(2));  
-    head = push(head, getNode(4));  
-    head = push(head, getNode(8));  
-    head = push(head, getNode(10));  
-  
-    System.out.print("Original list: ");  
-    printList(head);  
-  
-    // Reverse doubly linked list  
-    head = reverseList(head);  
-  
-    System.out.print("\nReversed list: ");  
-    printList(head);  
+public class reverseDoublyLinkedList { 
+
+    static Node head; 
+
+    static class Node { 
+
+        int data; 
+        Node next, prev; 
+
+        Node(int d) { 
+            data = d; 
+            next = prev = null; 
+        } 
+    } 
+
+    /* Function to reverse a Doubly Linked List */
+    void reverse() { 
+        Node temp = null; 
+        Node current = head; 
+
+        /* swap next and prev for all nodes of  
+         doubly linked list */
+        while (current != null) { 
+            temp = current.prev; 
+            current.prev = current.next; 
+            current.next = temp; 
+            current = current.prev; 
+        } 
+
+        /* Before changing head, check for the cases like empty  
+         list and list with only one node */
+        if (temp != null) { 
+            head = temp.prev; 
+        } 
+    } 
+
+    /* UTILITY FUNCTIONS */
+    /* Function to insert a node at the beginning of the Doubly Linked List */
+    void push(int new_data) { 
+        /* allocate node */
+        Node new_node = new Node(new_data); 
+
+        /* since we are adding at the beginning,  
+         prev is always NULL */
+        new_node.prev = null; 
+
+        /* link the old list off the new node */
+        new_node.next = head; 
+
+        /* change prev of head node to new node */
+        if (head != null) { 
+            head.prev = new_node; 
+        } 
+
+        /* move the head to point to the new node */
+        head = new_node; 
+    } 
+
+    /* Function to print nodes in a given doubly linked list  
+     This function is same as printList() of singly linked list */
+    void printList(Node node) { 
+        while (node != null) { 
+            System.out.print(node.data + " "); 
+            node = node.next; 
+        } 
+    } 
+
+    public static void main(String[] args) { 
+        reverseDoublyLinkedList list = new reverseDoublyLinkedList(); 
+
+        /* Let us create a sorted linked list to test the functions 
+         Created linked list will be 15->9->3->1 */
+        list.push(1); 
+        list.push(3); 
+        list.push(9); 
+        list.push(15); 
+
+        System.out.println("Original linked list "); 
+        list.printList(head); 
+
+        list.reverse(); 
+        System.out.println(""); 
+        System.out.println("The reversed Linked List is "); 
+        list.printList(head); 
+    } 
 } 
-}  
