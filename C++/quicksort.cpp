05a5dc78da588ef0d4f3048ecb19b105c56a9f85
Mohd Imran Ali <57518852+stark255@users.noreply.github.com>
2020-10-17 11:47:30 +0530

    updated quicksort

--- C++/quicksort.cpp
+++ C++/quicksort.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 /*--------------------------------------------------*/
 int partition(int a[],int l ,int h){
-	* This function takes last element as pivot, places 
+	/* This function takes last element as pivot, places 
    the pivot element at its correct position in sorted 
     array, and places all smaller (smaller than pivot) 
    to left of pivot and all greater elements to right 
