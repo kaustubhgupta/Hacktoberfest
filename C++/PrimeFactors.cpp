f65bddf840b0c2a66e5b3c39eed75b08a2eb96b4
AtalKhandelwal <54740592+AtalKhandelwal@users.noreply.github.com>
2020-10-18 07:34:00 +0530

    Prime Factors of a number

---
+++ C++/PrimeFactors.cpp
@@ -0,0 +1,27 @@
+//Program to find Prime Factors of any number.
+
+#include <iostream>
+
+using namespace std;
+
+void primeFactors(int number) 
+{ 
+        for (int i = 2; i <= number; i++)
+        {
+             while (number % i == 0)
+             {
+                  number /= i;
+                  cout <<  i << " ";  
+             }
+        }     
+ 
+} 
+
+int main()
+    {
+		cout << "Enter the number ";
+		int number;
+		cin >> number;
+        primeFactors(number);
+		return 0; 
+    }
