992cdbc239010488a46e3d625b1d05c1e6460208
Harish Chavan <harish.21810663@viit.ac.in>
2020-10-20 11:53:34 +0530

    Update Fahrenheit_to_celsius.py
    Updated docstring

--- Python/Fahrenheit_to_celsius.py
+++ Python/Fahrenheit_to_celsius.py
@@ -1,5 +1,6 @@
 def fahrenheit_to_celsius(start_temperature, end_temperature):          #Fucntion to convert Fahrenheit to Celsius accepting starting and ending value of range [start, end]
-  """number -> float
+  '''
+  number -> float
   
   Return temperature converted to °Celsius where input is in °Fahrenheit
   
@@ -13,7 +14,7 @@ def fahrenheit_to_celsius(start_temperature, end_temperature):          #Fucntio
   33°F is 0.5556°C
   34°F is 1.1111°C
   35°F is 1.6667°C
-  """
+  '''
   for fahrenheit in range (start_temperature, end_temperature + 1):
     print(str(fahrenheit)+'°F', 'is', str(round((fahrenheit - 32) * 5/9, ndigits=4))+'°C')  #Celsius = (Fahrenheit - 32) * 5/9  (Formula to convert temperature from °Fehrenheit to °Celsius)
     
