da254fe550948938b39576b32e68a2643f796452
preetiadvani1 <68547797+preetiadvani1@users.noreply.github.com>
2020-10-21 20:17:05 +0530

    Create Find whether the string or number is palindrome or not.java

---
+++ Find whether the string or number is palindrome or not.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class Palindrome {
+    public static void main (String[] args) {
+        String original, reverse = "";
+        Scanner in = new Scanner(System.in);
+        int length;
+        System.out.println("Enter the number or String");
+        original = in.nextLine();
+        length = original.length();
+        for (int i =length -1; i>;=0; i--) {
+            reverse = reverse + original.charAt(i);
+        }
+        System.out.println("reverse is:" +reverse);
+         
+        if(original.equals(reverse)) 
+            System.out.println("The number is palindrome");
+        else
+            System.out.println("The number is not a palindrome");
+         
+    }
+}
