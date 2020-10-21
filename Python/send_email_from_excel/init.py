9c94463aa0fa9cfd0fb7d74096af45da4e295bb8
= <xyzricky>
2020-10-21 22:12:20 +0530

    comments added

--- Python/send_email_from_excel/init.py
+++ Python/send_email_from_excel/init.py
@@ -2,8 +2,9 @@ import pandas as pd
 import sendEmail
 import os
 
+
 def fileNames():
-	#select file : excel
+	# This function will display the list of files inside the working directory
 	files = os.listdir(os.getcwd())
 	for k, v in enumerate(files):
 		print(k, v)
@@ -11,6 +12,7 @@ def fileNames():
 	return files
 
 def fileSelector():
+	# This function will encure that the user select one of the file inside the folder
 	files_ = fileNames()
 	f_ = int(input("Enter the file number: "))
 
@@ -26,26 +28,33 @@ def fileSelector():
 
 def start():
 	# configuration
-	USER_ = input('Enter your email: ')
-	PWD_ = input('Enter Password: ')
+	U_ = input('Enter your email: ')
+	P_ = input('Enter Password: ')
 	SENDER_ = input('Select one (Gmail/Outlook): ').capitalize()
 
-	if USER_ != "" or PWD_ != "" or SENDER_ != "":
+	# check if the user has entered all thr required information
+	if U_ != "" or P_ != "" or SENDER_ != "":
 
+		# check if the user has selected either gmail or outlook
 		if SENDER_ in ['Gmail','Outlook']:
 
-			EMAIL_ = sendEmail.Email(USER_, PASS_, SENDER_)
+			# configure the sendEmail module
+			EMAIL_ = sendEmail.Email(U_, P_, SENDER_)
 
+			# select the excel file
 			f = fileSelector()
 
 			try:
+				# read the excel file
 				data = pd.read_excel(f)
 
+				# loop over each rows in the excel file
 				for k, v in data.iterrows():
 					target = v['Email'] 
 					subject = v['Subject']
 					body = v['Body']
 
+					# send email 
 					e = EMAIL_.send(target, subject, body)
 
 					print(e)
