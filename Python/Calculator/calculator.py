d2d5cd05f6cc8dcc4d1a173dc147577455face8e
AyushBilkhiwal <69378143+AyushBilkhiwal@users.noreply.github.com>
2020-10-23 13:07:51 +0530

    updatedcalculator.py
    reduced the lines used by eliminating the input function that is used many times.

--- Python/Calculator/calculator.py
+++ Python/Calculator/calculator.py
@@ -1,63 +1,45 @@
 import math
 from builtins import print
 
-print ("*************************")
+print("*************************")
 
 op = input("Enter the operator: ").lower()
 
 # functions of the calculator
+firstNumber = float(input("Enter the first number: "))
+secondNumber = float(input("Enter the second number: "))
 if op == "+":
-    firstNumber = float(input("Enter the first number: "))
-    secondNumber = float(input("Enter the second number: "))
-    print (firstNumber, "+", secondNumber, "=", firstNumber + secondNumber )
-elif op == "-": 
-    firstNumber = float(input("Enter the first number: "))
-    secondNumber = float(input("Enter the second number: "))
-    print (firstNumber, "-", secondNumber, "=", firstNumber - secondNumber )
+    print(firstNumber, "+", secondNumber, "=", firstNumber + secondNumber)
+elif op == "-":
+    print(firstNumber, "-", secondNumber, "=", firstNumber - secondNumber)
 elif op == "*":
-    firstNumber = float(input("Enter the first number: "))
-    secondNumber = float(input("Enter the second number: "))
-    print (firstNumber, "*", secondNumber, "=", firstNumber * secondNumber )
+    print(firstNumber, "*", secondNumber, "=", firstNumber * secondNumber)
 elif op == "/":
-    firstNumber = float(input("Enter the first number: "))
-    secondNumber = float(input("Enter the second number: "))
-    print (firstNumber, "/", secondNumber, "=", firstNumber / secondNumber )
+    print(firstNumber, "/", secondNumber, "=", firstNumber / secondNumber)
 elif op == "^":
-    firstNumber = float(input("Enter the first number: "))
-    secondNumber = float(input("Enter the second number: "))
-    print (firstNumber, "^", secondNumber, "=", firstNumber ** secondNumber )
+    print(firstNumber, "^", secondNumber, "=", firstNumber ** secondNumber)
 elif op == "r":
-    firstNumber = float(input("Enter the first number: "))
-    secondNumber = float(input("Enter the second number: "))
-    print (firstNumber, "root", secondNumber, "=", secondNumber ** (1 / firstNumber) )
+    print(firstNumber, "root", secondNumber, "=", secondNumber ** (1 / firstNumber))
 elif op == "%":
-    firstNumber = float(input("Enter the first number: "))
-    secondNumber = float(input("Enter the second number: "))
-    print(firstNumber, "%", secondNumber, "=", firstNumber % secondNumber )
+    print(firstNumber, "%", secondNumber, "=", firstNumber % secondNumber)
 elif op == "!":
-    secondNumber = float(input("Enter the number: "))
-    theNumber = firstNumber = secondNumber 
+    theNumber = firstNumber = secondNumber
     secondNumber = 1
     while firstNumber > 1:
-        secondNumber *= firstNumber 
-        firstNumber = firstNumber - 1  
-    print ("n!(", theNumber, ")=", secondNumber )
+        secondNumber *= firstNumber
+        firstNumber = firstNumber - 1
+    print("n!(", theNumber, ")=", secondNumber)
 elif op == "sin":
-    secondNumber = float(input("Enter the number: "))
-    print ("sin(", secondNumber, ")=", math.sin(secondNumber ))
+    print("sin(", secondNumber, ")=", math.sin(secondNumber))
 elif op == "cos":
-    secondNumber = float(input("Enter the number: "))
-    print ("cos(", secondNumber, ")=", math.cos
-    (secondNumber ))
+    print("cos(", secondNumber, ")=", math.cos(secondNumber))
 elif op == "tan":
-    secondNumber = float(input("Enter the number: "))
-    print ("tan(", secondNumber, ")=", math.tan(secondNumber ))
+    print("tan(", secondNumber, ")=", math.tan(secondNumber))
 elif op == "pie" or op == "pi":
-    print ("Pie =", math.pi)
+    print("Pie =", math.pi)
 elif op == "e":
-    print = ("E =", math.e)
+    print("E =", math.e)
 elif op == "ln":
-    secondNumber = float(input("Enter the number: "))
-    print ("ln(", secondNumber , ")= ", math.log(secondNumber))
+    print("ln(", secondNumber, ")= ", math.log(secondNumber))
 else:
-    print ("incorrect operator") 
+    print("incorrect operator")
