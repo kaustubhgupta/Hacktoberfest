f27086e4244485d06379440c55dbb3335dfae88e
Blackwidow2981 <61281804+Blackwidow2981@users.noreply.github.com>
2020-10-29 21:08:02 +0530

    Merge Sort

--- Python/mergesort.py
+++ Python/mergesort.py
@@ -23,9 +23,9 @@ class LinkedList:
     def sortedMerge(self, a, b): 
         result = None
           
-        if a == None:  # Base cases  
+        if a is None:  # Base cases  
             return b 
-        if b == None: 
+        if b is None: 
             return a 
               
         if a.data <= b.data:  # pick either a or b  
@@ -39,7 +39,7 @@ class LinkedList:
       
     def mergeSort(self, h): 
           
-        if h == None or h.next == None:  # Base case if head is None 
+        if h is None or h.next is None:  # Base case if head is None 
             return h 
         middle = self.getMiddle(h)  # get the pointer to the middle of the list 
         nexttomiddle = middle.next
@@ -53,14 +53,14 @@ class LinkedList:
         return sortedlist 
         
     def getMiddle(self, head):  # Function to get the middle node of the linked list
-        if (head == None): 
+        if (head is None): 
             return head 
   
         slow = head 
         fast = head 
   
-        while (fast.next != None and 
-               fast.next.next != None): 
+        while (fast.next is not None and 
+               fast.next.next is not None): 
             slow = slow.next
             fast = fast.next.next
               
@@ -81,12 +81,12 @@ def printList(head):  # function to print the linked list
 if __name__ == '__main__': 
     li = LinkedList() 
       
-    li.append(15)  # Create an unsorted Linkedlist.
-    li.append(10) 
+    li.append(20)  # Create an unsorted Linkedlist.
+    li.append(60) 
     li.append(5) 
     li.append(20) 
     li.append(3) 
-    li.append(2) 
+    li.append(6) 
     li.head = li.mergeSort(li.head)  # Apply merge Sort  
     print ("Sorted Linked List is:") 
     printList(li.head) 
