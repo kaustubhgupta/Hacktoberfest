541e2eaa30d4bc840269b7eac6a72f9561ba01a7
Rahul Das <rahuldasjec.cse@gmail.com>
2021-10-02 09:48:46 +0530

    Modified, formatted and added comments in LinearSearch.java

--- Java/Searching/LinearSearch.java
+++ Java/Searching/LinearSearch.java
@@ -1,16 +1,34 @@
-public class LinearSearch{    
-public static int linearSearch(int[] arr, int key){  
-        // used  simple for loop to traverse each element to search
-        for(int i=0;i<arr.length;i++){    
-            if(arr[i] == key){    
-                return i;    
-            }    
-        }    
-        return -1;    
-    }    
-    public static void main(String args[]){    
-        int[] a1= {10,20,30,50,70,90};    
-        int key = 50;    
-        System.out.println(key+" is found at index: "+linearSearch(a1, key));    
-    }    
-}   
+/*
+* Linear search is the simplest search algorithm.
+* Starting from index 0, all the elements in the array are compared with target value sequentially.
+* Time complexity :
+        Best case : O(1)
+        Worst case : O(n)
+*/
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        // Here I have used a 'for loop' to compare the elements of the array with the target value.
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int[] a1 = {10, 1, 6, -100, 20, 63, 11, -5, 30, 50, 70, 100, 42, 0, 99, 7, 90};
+        System.out.println("Please enter an integer to search it in the array : ");
+        int target = input.nextInt();
+        int returnedindex = linearSearch(a1, target);
+        if (returnedindex == -1) {
+            System.out.println(target + " is not found in the array.");
+        } else {
+            System.out.println(target + " is found in the array at index " + returnedindex);
+        }
+    }
+}
