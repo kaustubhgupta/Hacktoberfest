eba4a26fd00c758d35edbe8a37c9b54b1bf6a544
SANDHI JAIN <73642560+sandhijain@users.noreply.github.com>
2021-10-02 23:15:19 +0530

    fahrenheit_to_celsius and celsius_to_fahrenheit
    I have added choice for the user  for choosing interconversion from fahrenheit to celsius or celsius to fahrenheit.

--- C/farenheit_to_celcius.c
+++ C/farenheit_to_celcius.c
@@ -1,15 +1,39 @@
 #include<stdio.h>
 int main(){
-    float celsius,fahrenheit;
+    float celsius, fahrenheit;
+    int choice;
 
-    //Taking input in fahrenheit
-    printf("\nEnter temperature in Fahrenheit: ");
-    scanf("%f",&fahrenheit);
- 
-    //Formula to convert temperature in fahrenheit to celcius
-    celsius=(fahrenheit - 32)*5/9;
-
-    //printing the temperature in celcius
-    printf("\nCelsius = %.3f\n",celsius);
-    return 0;
-}
+    //Taking input for conversion
+    printf("\nEnter:\t 1 - Fahrenheit to Celsius");
+    printf("\nEnter:\t 2 - Celsius to Fahrenheit");
+    printf("\nEnter your choice:\n");
+    scanf("%d",&choice);
+    
+    
+    //Taking input for temperature conversion
+    
+    switch(choice)
+    {
+         case 1:
+            printf("Enter the value of Fahrenheit temperature:");
+            scanf("%f", &fahrenheit);
+            //Formula to convert temperature in fahrenheit to celsius
+            celsius=(fahrenheit - 32)*5/9;
+            //printing the temperature in celcius
+            printf("\nCelsius = %.3f\n",celsius);
+            break;
+         case 2:
+            printf("Enter the value of Celsius temperature:\t");
+            scanf("%f", &celsius);
+            //Formula to convert temperature in celsius to fahrenheit
+            fahrenheit = celsius*(9/5) + 32;
+            //printing the temperature in fahrenheit
+            printf("\nFahrenheit= %.3f\n",fahrenheit);
+            break;
+         default:
+            printf("\n\nWrong Choice.....Try Again!!!\n");
+    }
+    
+         return 0;   
+    }           
+              
